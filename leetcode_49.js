8f47ec596c48becb66fe86c4c69f59f2e0140568
ekrambd <ekramhossainekram28@gmail.com>
2026-05-02 23:16:19 +0600

    leetcode 49 starting

---
+++ leetcode_49.js
@@ -0,0 +1,13 @@
+// 49. Group Anagrams
+var groupAnagrams = function(strs) {
+    let map = new Map();
+    for(let i = 0; i < strs.length; i++){
+        let str = strs[i];
+        let key = str.split('').sort().join('');
+        if(!map.has(key)) map.set(key,[]);
+        map.get(key).push(str);
+    }
+    return Array.from(map.values());
+};
+
+console.log(groupAnagrams(["bat","nat","tan","ate","eat","tea"]));
