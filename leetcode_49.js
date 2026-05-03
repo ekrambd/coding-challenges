758f1549ba6e8ce21b3e659651a45cc59857d18f
ekrmabd <ekramhossainekram28@gmail.com>
2026-05-03 11:59:14 +0600

    leetcode 49 solved

--- leetcode_49.js
+++ leetcode_49.js
@@ -1,13 +1,15 @@
 // 49. Group Anagrams
 var groupAnagrams = function(strs) {
     let map = new Map();
-    for(let i = 0; i < strs.length; i++){
-        let str = strs[i];
-        let key = str.split('').sort().join('');
-        if(!map.has(key)) map.set(key,[]);
-        map.get(key).push(str);
+    for(let i = 0; i < strs.length; i++)
+    {
+        let sorted = strs[i].split("").sort().join('');
+        if(!map.has(sorted)){
+            map.set(sorted,[]);
+        }
+        map.get(sorted).push(strs[i]);
     }
-    return Array.from(map.values());
+    return [...map.values()];        
 };
 
 console.log(groupAnagrams(["bat","nat","tan","ate","eat","tea"]));
